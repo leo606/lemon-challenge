77a0e8519ec7c3f1d60ee17f0ae930c52cbca910
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-03-21 16:17:47 -0300

    test: get client success response

--- tests/integration/clients.test.ts
+++ tests/integration/clients.test.ts
@@ -6,7 +6,10 @@ import "mocha";
 chai.use(chaiHttp);
 const expect = chai.expect;
 
-describe("get eligibilidade de cliente", () => {
+import clienteWrong from "./mocks/clientWrong";
+import clientCorrect from "./mocks/clientCorrect";
+
+describe("get eligibilidade status codes", () => {
   it("retorna status 400 - bad request ao não enviar dados do cliente", async () => {
     return chai
       .request(app)
@@ -14,6 +17,28 @@ describe("get eligibilidade de cliente", () => {
       .send({})
       .then((res) => {
         expect(res).to.have.status(400);
+      });
+  });
+
+  it("dados incorretos retorna status 400 - bad request", async () => {
+    return chai
+      .request(app)
+      .get("/")
+      .send(clienteWrong)
+      .then((res) => {
+        console.log(res.body);
+        
+        expect(res).to.have.status(400);
+      });
+  });
+
+  it("dados corretos retorna status 200 - ok", async () => {
+    return chai
+      .request(app)
+      .get("/")
+      .send(clientCorrect)
+      .then((res) => {
+        expect(res).to.have.status(200);
       });
   });
 });
