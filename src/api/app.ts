71c72b07e79c293dda9aeb00be7c5e0f8c2d6e4b
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-03-21 13:13:00 -0300

    joi client schema

--- src/api/app.ts
+++ src/api/app.ts
@@ -1,10 +1,14 @@
 import express, { Request, Response } from 'express';
+import clientSchema from '../helpers/joiSchemas';
 
 const app = express();
 
 app.use(express.json());
 
 app.get('/', async (req: Request, res:Response) => {
+  const client = req.body;
+  console.log(clientSchema.validate(client));
+
   res.status(200).send('express ts');
 });
 
---
+++ src/helpers/joiSchemas.ts
@@ -0,0 +1,19 @@
+import Joi from 'joi';
+
+const CpfRegex = /^\d{11}$/;
+const CnpjRegex = /^\d{14}$/;
+
+const clientSchema = Joi.object().keys({
+  numeroDoDocumento: Joi.alternatives().try(
+    Joi.string().regex(CpfRegex),
+    Joi.string().regex(CnpjRegex),
+  ).required(),
+  tipoDeConexao: Joi.string().valid('monofasico', 'bifasico', 'trifasico').required(),
+  classeDeConsumo: Joi.string().valid('residencial', 'industrial', 'comercial', 'rural', 'poderPublico').required(),
+  modalidadeTarifaria: Joi.string().valid('azul', 'branca', 'verde', 'convencional').required(),
+  historicoDeConsumo: Joi.array().items(
+    Joi.number().integer().min(0).max(9999),
+  ).min(3).max(12),
+});
+
+export default clientSchema;
