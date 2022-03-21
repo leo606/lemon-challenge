664b0ee6859f271435b8e353f20755c25307f342
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-03-21 16:02:45 -0300

    test: get client empty body

--- tests/integration/clients.test.ts
+++ tests/integration/clients.test.ts
@@ -1,19 +1,19 @@
-import app from '../../src/api/app';
-import chai from 'chai';
-import chaiHttp from 'chai-http';
-import 'mocha';
+import app from "../../src/api/app";
+import chai from "chai";
+import chaiHttp from "chai-http";
+import "mocha";
 
-chai.use(chaiHttp)
-const expect = chai.expect
+chai.use(chaiHttp);
+const expect = chai.expect;
 
 describe("get eligibilidade de cliente", () => {
-  describe("sem dados de cliente", () => {
-
-    it("retorna status 400 - bad request", () => {
-      return chai.request(app).get('/').send({})
-        .then(res=>{
-          expect(res).to.have.status(400)
-        })
-    });
+  it("retorna status 400 - bad request ao não enviar dados do cliente", async () => {
+    return chai
+      .request(app)
+      .get("/")
+      .send({})
+      .then((res) => {
+        expect(res).to.have.status(400);
+      });
   });
 });
