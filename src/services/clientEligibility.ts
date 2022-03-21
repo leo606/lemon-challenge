faaef4bbe7d067e8adc4f6dba658444ab087d733
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-03-21 18:47:23 -0300

    fix: move validations to validation module

---
+++ src/services/clientEligibility.ts
@@ -0,0 +1,48 @@
+import Client from '../interfaces/Client';
+
+function getLastTwelveMonths(consumptionHistory:number[]):number[] {
+  if (consumptionHistory.length <= 12) {
+    return consumptionHistory;
+  }
+  return consumptionHistory.splice(0, 12);
+}
+
+function getConsumptionAverage(consumptionHistory:number[]):number {
+  const amount = consumptionHistory.length;
+  const sum = consumptionHistory.reduce((acc, curr) => acc + curr);
+  return sum / amount;
+}
+
+function consumptionClassValidation(client:Client): boolean {
+  const accepted = ['comercial', 'residencial', 'industrial'];
+  return accepted.includes(client.classeDeConsumo);
+}
+
+function tariffValidation(client:Client): boolean {
+  const accepted = ['convencional', 'branca'];
+  return accepted.includes(client.modalidadeTarifaria);
+}
+
+function minConsumptionValidation(client:Client): boolean {
+  const consumptionHistory = getLastTwelveMonths(client.historicoDeConsumo);
+  const consumptionAverage = getConsumptionAverage(consumptionHistory);
+
+  const minConsumptionByConnType = {
+    monofasico: 400,
+    bifasico: 500,
+    trifasico: 750,
+  };
+
+  const minConsumption = minConsumptionByConnType[client.tipoDeConexao];
+  return consumptionAverage > minConsumption;
+}
+
+function clientEligibility(client:Client):boolean {
+  const consumptionValid = consumptionClassValidation(client);
+  const tariffValid = tariffValidation(client);
+  const minConsumptionValid = minConsumptionValidation(client);
+
+  return consumptionValid && tariffValid && minConsumptionValid;
+}
+
+export default clientEligibility;
--- src/services/clientReport.ts
+++ src/services/clientReport.ts
@@ -1,46 +1,14 @@
 import Client from '../interfaces/Client';
-
-function getLastTwelveMonths(consumptionHistory:number[]) {
-  if (consumptionHistory.length <= 12) {
-    return consumptionHistory;
-  }
-  return consumptionHistory.splice(0, 12);
-}
-
-function getConsumptionAverage(consumptionHistory:number[]) {
-  const amount = consumptionHistory.length;
-  const sum = consumptionHistory.reduce((acc, curr) => acc + curr);
-  return sum / amount;
-}
-
-function consumptionClassValidation(client:Client) {
-  const accepted = ['comercial', 'residencial', 'industrial'];
-  return accepted.includes(client.classeDeConsumo);
-}
-
-function tariffValidation(client:Client) {
-  const accepted = ['convencional', 'branca'];
-  return accepted.includes(client.modalidadeTarifaria);
-}
-
-function minConsumptionValidation(client:Client) {
-  const consumptionHistory = getLastTwelveMonths(client.historicoDeConsumo);
-  const consumptionAverage = getConsumptionAverage(consumptionHistory);
-
-  const minConsumptionByConnType = {
-    monofasico: 400,
-    bifasico: 500,
-    trifasico: 750,
-  };
-
-  const minConsumption = minConsumptionByConnType[client.tipoDeConexao];
-  return consumptionAverage > minConsumption;
-}
+import clientEligibility from './clientEligibility';
 
 function clientReport(client:Client): boolean {
-  console.log(consumptionClassValidation(client));
-  console.log(tariffValidation(client));
-  console.log(minConsumptionValidation(client));
+  const clientEligible = clientEligibility(client);
+
+  if (clientEligible) {
+    // gen positive report
+  } else {
+    // gen negative report
+  }
 
   return true;
 }
