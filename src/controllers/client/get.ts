70a0b594f12d50ef2dadc1eb47649fb994de0560
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-03-21 17:50:43 -0300

    feat: eligibility validations

--- src/controllers/client/get.ts
+++ src/controllers/client/get.ts
@@ -1,8 +1,15 @@
 import { Request, Response, Router } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import clientReport from '../../services/clientReport';
+
+import Client from '../../interfaces/Client';
 import clientValidation from '../../middlewares/clientValidation';
 
 async function getClientEligibility(req:Request, res:Response) {
+  const client:Client = req.body;
+
+  const report = clientReport(client);
+
   res.status(StatusCodes.NOT_IMPLEMENTED).end();
 }
 
