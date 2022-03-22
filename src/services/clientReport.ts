efd71f4e0a0080c9afa11bf1bb20100d5778ade7
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-03-22 10:00:55 -0300

    fix: remove comments

--- src/services/clientReport.ts
+++ src/services/clientReport.ts
@@ -3,19 +3,14 @@ import { EligibleReport, IneligibleReport } from '../interfaces/Reports';
 import { eligibilityValidation } from './eligibilityValidation';
 import { eligibleReport, ineligibleReport } from './genReports';
 
-function clientReport(client:Client): EligibleReport|IneligibleReport {
+function clientReport(client: Client): EligibleReport | IneligibleReport {
   const clientEligibility = eligibilityValidation(client);
-  let report: EligibleReport | IneligibleReport;
 
-  if (clientEligibility.every((validation:boolean) => validation)) {
-    // gen positive report
-    report = eligibleReport(client);
-  } else {
-    // gen negative report
-    report = ineligibleReport(clientEligibility);
+  if (clientEligibility.every((validation: boolean) => validation)) {
+    return eligibleReport(client);
   }
 
-  return report;
+  return ineligibleReport(clientEligibility);
 }
 
 export default clientReport;
