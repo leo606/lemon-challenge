3e196661234cd3771e0ee106a97997d0a60e0950
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-03-22 11:17:59 -0300

    feat: get report endpoint try catch

--- src/controllers/client/get.ts
+++ src/controllers/client/get.ts
@@ -1,16 +1,20 @@
-import { Request, Response, Router } from 'express';
+import {
+  Request, Response, Router, NextFunction,
+} from 'express';
 import { StatusCodes } from 'http-status-codes';
-import clientReport from '../../services/clientReport';
+import clientReport from '../../services/client/clientReport';
 
 import Client from '../../interfaces/Client';
 import clientValidation from '../../middlewares/clientValidation';
 
-async function getClientEligibility(req:Request, res:Response) {
-  const client:Client = req.body;
-
-  const report = clientReport(client);
-
-  res.status(StatusCodes.OK).json(report);
+async function getClientEligibility(req:Request, res:Response, next: NextFunction) {
+  try {
+    const client:Client = req.body;
+    const report = clientReport(client);
+    res.status(StatusCodes.OK).json(report);
+  } catch (error) {
+    next({ code: StatusCodes.INTERNAL_SERVER_ERROR, message: 'internal server error' });
+  }
 }
 
 export default (router: Router) => {
